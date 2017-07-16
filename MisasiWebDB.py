07cd8b8ec747679fd4074631103b8f299ba9ca87
robmisasi <Rob.misasi@gmail.com>
2017-07-15 20:07:57 -0500

    First commit

---
+++ MisasiWebDB.py
@@ -0,0 +1,34 @@
+import sqlite3 as lite
+import requests
+import datetime
+
+def get_house_temp():
+    r = requests.get('http://misasiweb.com/api/getHouseTemp')
+    data = r.json()
+    return data["temperature"]
+
+def init_db(db="mweb.db", table="temps"):
+    con = lite.connect(db)
+    with con:
+        cur = con.cursor()
+        cur.execute("CREATE TABLE " + table + "(Time TEXT, Temp INT)")
+
+def insert_temp(db="mweb.db", table="temps"):
+    temp = get_house_temp()
+
+    time_str = datetime.datetime.now().isoformat('T')
+    con = lite.connect(db)
+    with con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO " + table + " VALUES(\"" + time_str + "\" , " + str(temp) + ")")
+
+def print_all_temps(db="mweb.db", table="temps"):
+    con = lite.connect(db)
+
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM " + table)
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
